84bf32cea5ff5929f43e63e992d11a31d793de07
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2023-07-24 22:59:23 +0300

    Done!!!

--- src/pages/Facebook/Card/Post/index.js
+++ src/pages/Facebook/Card/Post/index.js
@@ -5,6 +5,9 @@ import ReplyOutlinedIcon from '@mui/icons-material/ReplyOutlined';
 import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
 import Modal from 'react-bootstrap/Modal';
 import Comments from './Comments';
+import Sentiment from 'sentiment';
+import { useEffect } from 'react';
+
 
 const formatTimestamp = (timestamp) => {
   const date = new Date(timestamp);
@@ -17,6 +20,42 @@ const formatTimestamp = (timestamp) => {
 
 function Post({ comments,media_type, description }) {
   const [modalShow, setModalShow] = React.useState(false);
+  const [commentResult, setCommentResult] = React.useState("");
+
+
+  useEffect(() => {
+    let positiveCount = 0;
+    let negativeCount = 0;
+
+    comments.forEach((comment) => {
+      const sentimentAnalyzer = new Sentiment();
+         // Perform sentiment analysis
+    const result = sentimentAnalyzer.analyze(comment?.message);
+    const result2 = result.score //0
+
+
+      if (result2 > 0) {
+        positiveCount++;
+        console.log('positive', positiveCount)
+      } else if (result2 < 0) {
+        negativeCount++;
+        console.log('negative', negativeCount)
+      }
+    });
+
+    if (positiveCount > negativeCount) {
+      setCommentResult("positive");
+    } else if (positiveCount < negativeCount) {
+      setCommentResult("negative");
+    } else {
+      setCommentResult("neutral");
+    }
+  
+  }, [comments]);
+
+
+
+
   return (
     <div>
     <CardActions
@@ -27,7 +66,15 @@ function Post({ comments,media_type, description }) {
     }}
   >
     <ThumbUpOutlinedIcon />
-    <ReplyOutlinedIcon />
+    <span>
+    {commentResult === "negative" ? (
+      <span style={{backgroundColor:'#E8E8E8',padding:3,borderRadius:5,fontWeight:'bold',color:'#FF5C5C'}} key="comment-basic-reply-to">{commentResult}</span>
+  ): commentResult === "positive" ? (
+    <span style={{backgroundColor:'#E8E8E8',padding:3,borderRadius:5,fontWeight:'bold',color:'#00FF00'}}  key="comment-basic-reply-to">{commentResult}</span>
+  ):(
+    <span style={{backgroundColor:'#E8E8E8',padding:3,borderRadius:5,fontWeight:'bold',color:'#A3A3A3'}} key="comment-basic-reply-to">{commentResult}</span>
+  )}
+    </span>
     <ForumOutlinedIcon onClick={() => setModalShow(true)} style={{cursor:'pointer'}}/>
   </CardActions>
 
@@ -45,7 +92,13 @@ function Post({ comments,media_type, description }) {
 >
   <Modal.Header closeButton>
     <Modal.Title id="contained-modal-title-vcenter">
-      {description}
+      {description} - {commentResult === "negative" ? (
+        <span style={{backgroundColor:'#E8E8E8',padding:3,borderRadius:5,fontWeight:'bold',fontSize:14, color:'#FF5C5C'}} key="comment-basic-reply-to">{commentResult}</span>
+    ): commentResult === "positive" ? (
+      <span style={{backgroundColor:'#E8E8E8',padding:3,borderRadius:5,fontWeight:'bold',fontSize:14, color:'#00FF00'}}  key="comment-basic-reply-to">{commentResult}</span>
+    ):(
+      <span style={{backgroundColor:'#E8E8E8',padding:3,borderRadius:5,fontWeight:'bold',fontSize:14, color:'#A3A3A3'}} key="comment-basic-reply-to">{commentResult}</span>
+    )}
     </Modal.Title>
   </Modal.Header>
   <Modal.Body>
